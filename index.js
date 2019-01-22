7807f1fa743160bcc19ae060a6689df02f126f7d
mikka <mikkanthrope@gmail.com>
2019-01-22 03:57:31 -0800

    Add swipe interactions

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 // GLOBALS
 
-var selected;
+var currentNode = document.documentElement;
+var currentId;
+var selectedTitle;
+var rect = currentNode.getBoundingClientRect();
+var originX = 0;
+var startX = 0;
+var endX = 0;
+var destinationX = 0;
+var list;
 
 // IMAGE ASSETS - to be replaced with API
 
@@ -26,33 +34,32 @@ const footerIcons = [
 
 const tools = document.getElementById('tools')
 tools.onclick = function () {
-  this.classList.remove('showOverlay')
+  this.classList.remove('showOverlay');
 }
 
-const backBtn = document.createElement("DIV")
-let action = document.createElement("IMG")
+const backBtn = document.createElement('DIV')
+let action = document.createElement('IMG')
 action.src = 'icons/back.svg'
 action.id = 'backBtn'
 backBtn.appendChild(action)
+backBtn.onclick = (event) => { closeSlide(); }
 
-const closeBtn = document.createElement("H1")
+const closeBtn = document.createElement('H1')
 closeBtn.className = 'closeBtn'
 closeBtn.innerHTML = '&#10005;'
-closeBtn.onclick = function () {
-  document.getElementsByClassName('isAnimating')[0].classList.remove('isAnimating')
-}
+closeBtn.onclick = (event) => { closeSlide(); }
 
 const titleText = (text='default') => {
-  let photoTitle = document.createElement("DIV")
-  let h1 = document.createElement("H1")
+  let photoTitle = document.createElement('DIV')
+  let h1 = document.createElement('H1')
   let textNode = document.createTextNode(text)
-  h1.appendChild(textNode);
+  h1.appendChild(textNode)
   photoTitle.appendChild(h1)
   photoTitle.id = 'titleText'
-  return photoTitle
+  return photoTitle;
 }
 
-const titlebar = document.createElement("DIV")
+const titlebar = document.createElement('DIV')
 titlebar.id = 'titlebar'
 titlebar.className = 'tool titlebar'
 titlebar.appendChild(backBtn)
@@ -60,10 +67,10 @@ titlebar.appendChild(titleText())
 titlebar.appendChild(closeBtn)
 document.getElementById('tools').appendChild(titlebar)
 
-const footer = document.createElement("DIV")
+const footer = document.createElement('DIV')
 footer.className = 'tool footer'
 footerIcons.forEach(icon => {
-  let action = document.createElement("IMG")
+  let action = document.createElement('IMG')
   action.src = icon.url
   action.id = icon.name
   footer.appendChild(action)
@@ -74,20 +81,89 @@ document.getElementById('tools').appendChild(footer)
 
 photos.forEach(photo => {
   // create tile to contain image
-  let tile = document.createElement("DIV")
+  let tile = document.createElement('DIV')
   tile.id = photo.id
   tile.title = photo.name
   tile.className = 'tile'
   tile.setAttribute('role', 'img')
   tile.setAttribute('aria-label', photo.name)
-  tile.style.backgroundImage = "url(" + photo.url + ")"
+  tile.style.backgroundImage = 'url(' + photo.url + ')'
+  document.getElementById('gallery').appendChild(tile)
   // handle clicks
   tile.onclick = function (event) {
-    selected = event.target.title
-    let newTitle = titleText(selected)
+    currentId = event.target.id;
+    selectedTitle = event.target.title
+    let newTitle = titleText(selectedTitle)
     document.getElementById('titlebar').replaceChild(newTitle, document.getElementById('titleText'))
-    this.classList.add('isAnimating')
+    document.documentElement.style.overflow = 'hidden'
+    this.classList.add('openSlide')
     tools.classList.add('showOverlay')
-  };
-  document.getElementById('gallery').appendChild(tile)
+    // handle swipe
+    this.ontouchstart = (event) => { handleTouchStart(event); }
+    this.ontouchmove = (event) => { handleTouchMove(event); }
+    this.ontouchend = (event) => { handleTouchEnd(event); }
+  }
 })
+
+// INTERACTIONS
+
+getNextImage = () => {
+  currentNode.classList.toggle('openSlide')
+  currentNode.style.transform = null
+  let next = document.getElementById(currentId)
+  next.classList.add('openSlide')
+  next.ontouchstart = (event) => { handleTouchStart(event) }
+  next.ontouchmove = (event) => { handleTouchMove(event) }
+  next.ontouchend = (event) => { handleTouchEnd(event) }
+}
+
+closeSlide = () => {
+  event.preventDefault()
+  document.getElementsByClassName('openSlide')[0].classList.remove('openSlide')
+  document.documentElement.style.overflow = 'auto'
+}
+
+handleTouchStart = (event) => {
+  currentNode = document.getElementById(event.target.id)
+  rect = currentNode.getBoundingClientRect()
+  // record where finger first touched screen
+  startX = event.touches[0].clientX
+  // set starting point for left position of selected image
+  originX = startX - rect.left
+}
+
+handleTouchMove = (event) => {
+  for(var i=0; i<event.touches.length; i++) {
+    endX = event.touches[i].clientX
+    destinationX = endX - rect.left
+    currentNode.style.transform = `translate3d(${endX-startX}px, 0, 0)`
+  }
+  originX = 0
+}
+
+handleTouchEnd = (event) => {
+  if (destinationX === 0) { return; }
+  if (Math.abs(endX - startX) > document.getElementById('gallery').clientWidth/6) {
+    if ((endX - startX) > 0) {
+      // if swipe right, go forward one image
+      if (currentId == photos.length) {
+        currentId = 1
+      } else {
+        currentId ++
+      }
+    } else if ((endX - startX) < 0) {
+      // if swipe left, go back one image
+      if (currentId == 1) {
+        currentId = photos.length
+      } else {
+        currentId --
+      }
+    }
+    getNextImage()
+  } else {
+    // snap back into original place if swipe accidental
+    destinationX = 0
+    currentNode.style.transform = `translate3d(${-(destinationX-originX)}px, 0, 0)`
+    currentNode.style.position = 'absolute'
+  }
+}
